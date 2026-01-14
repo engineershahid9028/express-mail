d2feaf780ddbec649121cb0eeda1c4ec4491f08b
engineershahid9028 <141816607+engineershahid9028@users.noreply.github.com>
2026-01-14 21:09:27 +0500

    Delete geo.py

--- geo.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def detect_country(ip):
-    try:
-        res = requests.get(f"https://ipapi.co/{ip}/json/").json()
-        return res.get("country_code", "US")
-    except:
-        return "US"
