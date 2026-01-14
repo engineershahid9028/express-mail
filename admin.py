60e7403114657ff685a581b3361b3b82bda26218
engineershahid9028 <141816607+engineershahid9028@users.noreply.github.com>
2026-01-14 21:09:09 +0500

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-import os
-from fastapi import HTTPException
-
-ADMIN_KEY = os.getenv("ADMIN_KEY")
-
-def verify_admin(key):
-    if key != ADMIN_KEY:
-        raise HTTPException(403, "Unauthorized")
